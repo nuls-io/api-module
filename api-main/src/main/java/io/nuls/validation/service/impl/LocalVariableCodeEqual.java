82d2f3ecc43c97b4f3f73cb9207cfdbc09c04dc0
MIMIEYES <pierreluohao@gmail.com>
2019-01-24 11:05:48 +0800

    contract code validation

--- api-main/src/main/java/io/nuls/validation/service/impl/LocalVariableCodeEqual.java
+++ api-main/src/main/java/io/nuls/validation/service/impl/LocalVariableCodeEqual.java
@@ -1,14 +1,18 @@
-/*
+/**
  * MIT License
- * Copyright (c) 2017-2019 nuls.io
+ * <p>
+ * Copyright (c) 2017-2018 nuls.io
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
+ * <p>
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -17,47 +21,36 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+package io.nuls.contract.validation.service.impl;
 
-package io.nuls.api.controller.model;
+import io.nuls.contract.validation.service.ObjectEqual;
+import io.nuls.contract.vm.code.LocalVariableCode;
 
 /**
- * @author Niels
+ * @author: PierreLuo
+ * @date: 2018/12/26
  */
-public enum RpcErrorCode {
-    // 参数不对
-    PARAMS_ERROR(1000, "Parameters is wrong!"),
+public class LocalVariableCodeEqual implements ObjectEqual<LocalVariableCode> {
 
-    //数据未找到
-    DATA_NOT_EXISTS(404, "Data is not exists!"),
+    private static final LocalVariableCodeEqual LOCAL_VARIABLE_CODE_EQUAL = new LocalVariableCodeEqual();
 
-    //交易解析错误
-    TX_PARSE_ERROR(999, "Transaction parse error!"),
-
-    //脚本执行错误
-    TX_SHELL_ERROR(755, "Shell execute error!");
-
-
-    private int code;
-
-    private String message;
-
-    RpcErrorCode(int code, String message) {
-        this.code = code;
-        this.message = message;
+    private LocalVariableCodeEqual() {
     }
 
-    public int getCode() {
-        return code;
+    public static LocalVariableCodeEqual getInstance() {
+        return LOCAL_VARIABLE_CODE_EQUAL;
     }
 
-    public void setCode(int code) {
-        this.code = code;
-    }
+    @Override
+    public boolean customizeEquals(LocalVariableCode thisO, LocalVariableCode o) {
+        if (thisO == o) return true;
+        if (!(o instanceof LocalVariableCode)) return false;
 
-    public String getMessage() {
-        return message;
-    }
+        LocalVariableCode that = (LocalVariableCode) o;
 
-    public void setMessage(String message) {
-        this.message = message;
-    }}
+        if (thisO.name != null ? !thisO.name.equals(that.name) : that.name != null) return false;
+        if (thisO.desc != null ? !thisO.desc.equals(that.desc) : that.desc != null) return false;
+
+        return true;
+    }
+}
--- api-main/src/main/java/io/nuls/validation/service/impl/ParameterNodeEqual.java
+++ api-main/src/main/java/io/nuls/validation/service/impl/ParameterNodeEqual.java
@@ -1,14 +1,18 @@
-/*
+/**
  * MIT License
- * Copyright (c) 2017-2019 nuls.io
+ * <p>
+ * Copyright (c) 2017-2018 nuls.io
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
+ * <p>
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -17,47 +21,36 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+package io.nuls.contract.validation.service.impl;
 
-package io.nuls.api.controller.model;
+import io.nuls.contract.validation.service.ObjectEqual;
+import org.objectweb.asm.tree.ParameterNode;
 
 /**
- * @author Niels
+ * @author: PierreLuo
+ * @date: 2018/12/26
  */
-public enum RpcErrorCode {
-    // 参数不对
-    PARAMS_ERROR(1000, "Parameters is wrong!"),
+public class ParameterNodeEqual implements ObjectEqual<ParameterNode> {
 
-    //数据未找到
-    DATA_NOT_EXISTS(404, "Data is not exists!"),
+    private static final ParameterNodeEqual PARAMETER_NODE_EQUAL = new ParameterNodeEqual();
 
-    //交易解析错误
-    TX_PARSE_ERROR(999, "Transaction parse error!"),
+    private ParameterNodeEqual(){}
 
-    //脚本执行错误
-    TX_SHELL_ERROR(755, "Shell execute error!");
-
-
-    private int code;
-
-    private String message;
-
-    RpcErrorCode(int code, String message) {
-        this.code = code;
-        this.message = message;
+    public static ParameterNodeEqual getInstance() {
+        return PARAMETER_NODE_EQUAL;
     }
 
-    public int getCode() {
-        return code;
+    @Override
+    public boolean customizeEquals(ParameterNode thisO, ParameterNode o) {
+        if (thisO == o) return true;
+        if (!(o instanceof ParameterNode)) return false;
+
+        ParameterNode that = (ParameterNode) o;
+
+        if (thisO.access != that.access) return false;
+        if (thisO.name != null ? !thisO.name.equals(that.name) : that.name != null) return false;
+
+        return true;
     }
 
-    public void setCode(int code) {
-        this.code = code;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }}
+}
